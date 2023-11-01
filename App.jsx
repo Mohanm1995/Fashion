cee5251de35a6bb68a2c11138e4c78fbefd9abca
Mohanm1995 <120558975+Mohanm1995@users.noreply.github.com>
2023-11-01 15:44:08 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Fashion from "./Components/Fashion/Fashion";
+
+const App = () => {
+  return (
+    <div>
+      <Fashion />
+    </div>
+  );
+};
+
+export default App;
